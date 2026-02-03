07815f1fad84fbbda65cdaf1eb45e75668c94778
Kadyn <kadyntrobotics@gmail.com>
2026-02-02 19:30:35 -0500

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,6 @@
 import { useQRScoutState } from '@/store/store';
-
+import { Heart } from 'lucide-react';
+import { Logo } from './Logo';
 
 export function Footer() {
   const teamNumber = useQRScoutState(state => state.formData.teamNumber);
@@ -7,8 +8,14 @@ export function Footer() {
     <footer>
       <div className="mt-8 flex flex-col items-center justify-center p-2 gap-2">
         <div className="h-24 w-96">
-        <img src="src/assets/7226_logo_animated2.svg" alt="WavOps Logo">
+          <Logo />
         </div>
+        {teamNumber !== 2713 && (
+          <>
+            <Heart className="text-primary size-8 fill-primary" />
+            <span className="text-2xl text-primary font-rhr-ns">
+              {teamNumber}
+            </span>
           </>
         )}
       </div>
