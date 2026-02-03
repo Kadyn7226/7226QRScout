876362e9f251f06fbbd4a51e6b42862730775904
Kadyn <kadyntrobotics@gmail.com>
2026-02-02 19:32:43 -0500

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,6 +8,7 @@ export function Footer() {
     <footer>
       <div className="mt-8 flex flex-col items-center justify-center p-2 gap-2">
         <div className="h-24 w-96">
+         <img src="src/assets/7226_logo_animated2.svg"></img>
           <Logo />
         </div>
         {teamNumber !== 2713 && (
