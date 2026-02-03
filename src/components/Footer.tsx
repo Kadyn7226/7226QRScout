76677f51cd18aa438ab4d9e68b3571cb27884622
Kadyn <kadyntrobotics@gmail.com>
2026-02-02 19:42:06 -0500

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,21 @@
+// src/components/Footer.tsx
 import { useQRScoutState } from '@/store/store';
 import { Heart } from 'lucide-react';
-import { Logo } from './src/assets/7226_logo_animated2.svg';
+import { Logo } from './Logo';
 
 export function Footer() {
   const teamNumber = useQRScoutState(state => state.formData.teamNumber);
+  
   return (
     <footer>
       <div className="mt-8 flex flex-col items-center justify-center p-2 gap-2">
+        {/* This container controls the size of your SVG */}
         <div className="h-24 w-96">
           <Logo />
         </div>
-        {teamNumber !== 2713 && (
+        
+        {/* Conditionally render the heart and team number */}
+        {teamNumber !== 7226 && (
           <>
             <Heart className="text-primary size-8 fill-primary" />
             <span className="text-2xl text-primary font-rhr-ns">
