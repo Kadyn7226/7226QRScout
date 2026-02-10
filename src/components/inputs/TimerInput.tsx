6551f868f51fe8b612e2e613d483368f7afdcd88
Kadyn <kadyntrobotics@gmail.com>
2026-02-09 19:08:40 -0500

    Update TimerInput.tsx

--- src/components/inputs/TimerInput.tsx
+++ src/components/inputs/TimerInput.tsx
@@ -137,7 +137,7 @@ export default function TimerInput(props: ConfigurableInputProps) {
             <Play className="size-4" />
           )}
         </Button>
-      <Button variant="outline" disabled={true} onClick={lap}>
+    
           <TimerReset className="size-4" />
         </Button>
         <Button variant="outline" onClick={() => resetState({ force: false })}>
